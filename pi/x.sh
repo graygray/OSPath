2ce67bb67ac2fc9b0cb57fde1732b3bd68bab4c9
gray.lin <gray.lin@primax.com.tw>
2024-01-05 14:42:12 +0800

    update pi

--- pi/x.sh
+++ pi/x.sh
@@ -8,6 +8,206 @@ echo "param 3:"$3
 echo "param 4:"$4
 echo "param 5:"$5
 
+WorkingDir="/home/pi/lcd"
+emojiDir="$WorkingDir/LCD"
+emojiDir2="$WorkingDir/LCD_timeless"
+emojiFile="Agree.mp4"
+emojiFile2="Blink.mp4"
+emojiFiles=(Agree Blink Confused Cute Disagree Dizzy Happy_1 Shock SleepLoop Sleep Smile StartSleep Talk Tired WakeUp2)
+
+rosDir_Home="/opt/ros/galactic"
+testNode="lcd_set_emoji"
+nodeDir="$WorkingDir/$testNode"
+
+# ROS
+if [ "$1" = "ros" ] ; then
+	echo "========== ROS =========="
+
+	if [  "$2" = "n" ] ; then
+		echo "========== node =========="
+		cd $nodeDir
+		if [  "$3" = "i" ] ; then
+			echo "ros2 node info $4"
+			ros2 node info $4
+		elif [  "$3" = "r" ] ; then
+			echo "ros2 run $testNode "$testNode"_node"
+			source install/setup.sh
+			ros2 run $testNode "$testNode"_node
+		elif [ "$3" = "b" ] ; then
+			echo "========== colcon build =========="
+			cd $nodeDir
+			colcon build
+			# colcon build --packages-select lcd_set_emoji
+		else
+			ros2 node list
+		fi
+	elif [ "$2" = "env" ] ; then
+		source /opt/ros/galactic/setup.bash
+
+	elif [ "$2" = "p" ] ; then
+		echo "========== pkg =========="
+		if [  "$3" = "exe" ] ; then
+			if [ -z "$4" ] ; then
+				ros2 pkg executables
+			else
+				ros2 pkg executables $4
+			fi
+		elif [ "$3" = "prefix" ] ; then
+			echo "ros2 pkg prefix <package-name>"
+			ros2 pkg prefix $4
+		elif [ "$3" = "xml" ] ; then
+			echo "ros2 pkg xml <package-name>"
+			ros2 pkg xml $4
+		elif [ "$3" = "c" ] ; then
+			echo "ros2 pkg create $4"
+			ros2 pkg create $4 --build-type ament_cmake --dependencies rclcpp
+		else 
+			echo "ros2 pkg list"
+			ros2 pkg list
+		fi
+
+	elif [ "$2" = "tp" ] ; then
+		testTopic="/chatter"
+		testTopic2="/chatter2"
+		echo "========== topic:$testTopic =========="
+
+		if [  "$3" = "p" ] ; then
+			echo "publish..."
+			ros2 topic pub $testTopic std_msgs/msg/String 'data: "test"'
+		elif [ "$3" = "e" ] ; then
+			echo "echo..."
+			ros2 topic echo $testTopic2
+		else 
+			echo "ros2 topic list -t"
+			ros2 topic list -t
+		fi
+
+	elif [ "$2" = "i" ] ; then
+		echo "========== interface =========="
+		if [  "$3" = "p" ] ; then
+			echo "ros2 interface packages"
+			ros2 interface packages
+		elif [ "$3" = "x" ] ; then
+			echo "xxx..."
+		else 
+			echo "ros2 interface list"
+			ros2 ros2 interface list
+		fi
+
+	elif [ "$2" = "s" ] ; then
+		echo "========== service =========="
+		if [  "$3" = "r" ] ; then
+			echo "run service"
+			ros2 run examples_rclpy_minimal_service service
+		elif [ "$3" = "x" ] ; then
+			echo "xxx..."
+		else 
+			echo "ros2 service list"
+			ros2 service list
+		fi
+
+	elif [ "$2" = "param" ] ; then
+
+		if [  "$3" = "r" ] ; then
+			echo "run service"
+			ros2 run examples_rclpy_minimal_service service
+		elif [ "$3" = "set" ] ; then
+			echo "set..."
+			ros2 param set /lcd_set_emoji param_emoji_name "$4"
+		else 
+			echo "ros2 param list"
+			ros2 param list
+		fi
+
+	fi
+fi
+
+
+if [ "$1" = "emoji" ] ; then
+	export XDG_RUNTIME_DIR=/run/user/root
+	PID_2kill=`cat $WorkingDir/PID_2kill`
+	PID_last=`cat $WorkingDir/PID_last`
+	run_counter=`cat $WorkingDir/run_counter`
+
+	if [ "$2" = "kill" ] ; then
+		echo "kill..."
+		# kill -9 $PID_2kill
+		# kill -9 $PID_last
+		pkill gst*
+
+	elif [ "$2" = "clean" ] ; then
+		echo "clean..."
+		rm $WorkingDir/PID_*
+		rm run_counter
+		rm -rf lcd_set_emoji
+
+	elif [ "$2" = "git" ] ; then
+		echo "git clone..."
+		cd $WorkingDir
+		rm -rf lcd_set_emoji
+		git clone ssh://git@10.1.7.125:10022/Gray.LIn/lcd_set_emoji.git
+
+	elif [ "$2" = "ls" ] ; then
+		echo "list emoji..."
+		ls -al $emojiDir2
+
+	else
+		echo "Run emoji $2.mp4"
+		if [ -z "$PID_2kill" ] ; then
+			echo "PID_2kill empty"
+		else
+			echo PID_2kill:$PID_2kill
+		fi
+		if [ -z "$PID_last" ] ; then
+			echo "PID_last empty"
+		else
+			echo PID_last:$PID_last
+		fi
+
+		if [ "$3" = "full" ] ; then
+			gst-launch-1.0 multifilesrc location=$emojiDir2/$2.mp4 loop=true ! decodebin ! waylandsink fullscreen=TRUE&
+		elif [ "$3" = "720" ] ; then
+			echo "720P"
+			gst-launch-1.0 multifilesrc location=$emojiDir2/$2.mp4 loop=true ! decodebin ! waylandsink x=20 y=20 width=1280 height=720&
+		elif [ "$3" = "1080" ] ; then
+			echo "1080P"
+			gst-launch-1.0 multifilesrc location=$emojiDir2/$2.mp4 loop=true ! decodebin ! waylandsink x=20 y=20 width=1920 height=1080&
+		else
+			gst-launch-1.0 multifilesrc location=$emojiDir2/$2.mp4 loop=true ! decodebin ! waylandsink&
+		fi
+
+		# if [ -z "$run_counter" ] ; then
+		# 	echo "run_counter empty"
+		# 	$run_counter = "0"
+		# else
+		# 	echo run_counter:$run_counter
+		# 	if [ $run_counter -gt 5 ] ; then
+		# 		run_counter=0
+		# 		pkill gst*
+		# 	else
+		# 		run_counter=$(($run_counter+"1"))
+		# 	fi
+		# fi
+		# echo $run_counter > $WorkingDir/run_counter
+
+		echo PID_last:$PID_last
+		PID_2kill=$PID_last
+		PID_last=$!
+		echo $PID_2kill > $WorkingDir/PID_2kill
+		echo $PID_last > $WorkingDir/PID_last
+		if [ -z "$PID_2kill" ] ; then
+			echo "PID_2kill empty"
+		else
+			sleep 1
+			echo "kill -9 $PID_2kill"
+			kill -9 $PID_2kill
+		fi
+
+	fi
+
+fi
+
+
 # system related 
 if [ "$1" == "sys" ] ; then
 	if [ "$2" == "service" ] ; then
@@ -42,6 +242,13 @@ if [ "$1" == "sys" ] ; then
 		exit -1
 	fi
 fi
+
+# update x
+if [ "$1" == "ux" ] ; then
+	cd /home/pi/OSPath
+	git pull
+fi
+
 
 # gedit
 if [ "$1" == "ge" ] ; then
