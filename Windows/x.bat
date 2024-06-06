3e3791ebd6af4de464fcbd6296f20b019a964d95
gray.lin <gray.lin@primax.com.tw>
2024-06-06 13:25:59 +0800

    u

--- Windows/x.bat
+++ Windows/x.bat
@@ -3,17 +3,23 @@
 cd %~dp0
 
 set arg1=%1
-
-if "%arg1%"=="xxxxx" (
+set arg2=%2
+set arg3=%3
 echo %arg1%
-echo in if
+echo %arg2%
+echo %arg3%
+
+
+if "%arg1%"=="stm" (
+    echo %arg1%
+) 
+
+if "%arg1%"=="code" (
+
+    if "%arg2%"=="stmlive" (
+        code C:\Users\gray.lin\STM32CubeIDE\workspace_1.13.2\.metadata\.plugins\com.st.stm32cube.ide.mcu.livewatch\saved_expr.dat
+    )
 )
-
-echo line3
-
-
-IF DEFINED arg1 echo %arg1%
-
 
 
 pause
