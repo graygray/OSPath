3aa8747ca0c1a2bac9a47e5918a6dd58329cf38b
Gray Lin <gray.lin@primax.com.tw>
2026-09-16 19:27:12 +0800

    Configure feedback RPM resolution for motor control

--- AICamera/x.sh
+++ AICamera/x.sh
@@ -1678,7 +1678,8 @@ if [ "$1" = "aic" ]; then
 				--params-file "$motor_control_params" \
 				-p enable_can:=true \
 				-p can_interface:="$can_interface" \
-				-p rpm_resolution:=1.0
+				-p rpm_resolution:=1.0 \
+				-p feedback_rpm_resolution:=0.1
 		elif [ "$3" = "i" ]; then
 			ipk_file="motor-control-g4dual_0.1.0-r0_armv8a.ipk"
 			echo "opkg install $ipk_file --force-reinstall"
